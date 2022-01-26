f51058b25e787e699677a2ca393be54c1ab94d14
ecemcakmak <eng.ecemcakmak@gmail.com>
2022-01-26 15:15:05 +0300

    start: starting to my first tailwind attemp

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./src/**/*.{html,js}',
+'./index.html'],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('/img/hero-pattern.svg')",
+        'footer-texture': "url('/img/footer-texture.png')",
+      }
+    },
+  },
+  plugins: [],
+}
